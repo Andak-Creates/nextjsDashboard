3d394c5faaa8549d7a41dcd2d6e17ad7ccd19b3c
Vindi <kelvinandak@gmail.com>
2025-03-26 04:09:07 +0100

    finished tutorial

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,6 +1,6 @@
-import CardWrapper, { Card } from "@/app/ui/dashboard/cards";
-import RevenueChart from "@/app/ui/dashboard/revenue-chart";
-import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
+import CardWrapper from "@/app/ui/dashboard/cards";
+// import RevenueChart from "@/app/ui/dashboard/revenue-chart";
+// import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import { lusitana } from "@/app/ui/fonts";
 import { Metadata } from "next";
 import { Suspense } from "react";
